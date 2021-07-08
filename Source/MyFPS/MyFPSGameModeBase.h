62bf04cf3285af22fd7e755e02cae66152050617
JarrettCai <875306324@qq.com>
2021-07-08 15:32:36 +0800

    + 初始化项目文件

---
+++ Source/MyFPS/MyFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFPS_API AMyFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
